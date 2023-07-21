1721c1535dd49dfe5fc284fe043927d42504f723
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-07-21 16:05:59 -0500

    Created productopolis api

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # productopolis
+
 In here there are a product management API which handles info about products 
---
+++ src/main/java/co/crisi/productopolis/ProductopolisApplication.java
@@ -0,0 +1,13 @@
+package co.crisi.productopolis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductopolisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductopolisApplication.class, args);
+    }
+
+}
