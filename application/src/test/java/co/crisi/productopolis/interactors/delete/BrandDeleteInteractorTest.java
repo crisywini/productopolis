db33b662d65f52efec800e554da12e3be3d8f2f0
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-07-25 15:06:44 -0500

    ref

--- application/src/test/java/co/crisi/productopolis/interactors/delete/BrandDeleteInteractorTest.java
+++ application/src/test/java/co/crisi/productopolis/interactors/delete/BrandDeleteInteractorTest.java
@@ -3,6 +3,7 @@ package co.crisi.productopolis.interactors.delete;
 import co.crisi.productopolis.boundaries.input.delete.IBrandDeleteBoundary;
 import co.crisi.productopolis.boundaries.output.IBrandDeleteGateway;
 import co.crisi.productopolis.exception.BrandBusinessException;
+import co.crisi.productopolis.exception.BrandNotFoundException;
 import co.crisi.productopolis.presenter.delete.IBrandDeletePresenter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
@@ -46,6 +49,29 @@ class BrandDeleteInteractorTest {
 
         assertThat(idDeleted)
                 .isEqualTo(id);
+
+    }
+
+    @Test
+    void deleteByIdWhenNonExistingBrand_shouldPrepareFailView() throws BrandBusinessException {
+        var id = 1L;
+        var throwable = new BrandNotFoundException(String.format("The brand with id %d was not found!", id));
+        given(gateway.existsById(id))
+                .willReturn(false);
+
+        given(presenter.prepareFailView(any(BrandBusinessException.class)))
+                .willThrow(throwable);
+
+        var response = catchThrowable(() -> {
+            var idDeleted = boundary.deleteById(id);
+        });
+
+        verify(gateway).existsById(id);
+        verify(presenter).prepareFailView(any(BrandBusinessException.class));
+
+        assertThat(response)
+                .isInstanceOf(BrandNotFoundException.class)
+                .isEqualTo(throwable);
 
     }
 
