b1a347d34fb347c5c4da971c7b53b819381bfb29
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-07-26 15:43:58 -0500

    test attribute register boundary

--- application/src/main/java/co/crisi/productopolis/model/response/mapper/AttributeMapper.java
+++ application/src/main/java/co/crisi/productopolis/model/response/mapper/AttributeMapper.java
@@ -1,6 +1,7 @@
 package co.crisi.productopolis.model.response.mapper;
 
 import co.crisi.productopolis.domain.IAttribute;
+import co.crisi.productopolis.model.request.AttributeRequest;
 import co.crisi.productopolis.model.response.AttributeResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -13,5 +14,11 @@ public interface AttributeMapper {
     @Mapping(target = "description", expression = "java(attribute.getDescription())")
     @Mapping(target = "value", expression = "java(attribute.getValue())")
     AttributeResponse map(IAttribute attribute);
+
+    @Mapping(target = "id", expression = "java(attribute.getId())")
+    @Mapping(target = "name", expression = "java(attribute.getName())")
+    @Mapping(target = "description", expression = "java(attribute.getDescription())")
+    @Mapping(target = "value", expression = "java(attribute.getValue())")
+    AttributeRequest mapToRequest(IAttribute attribute);
 
 }
