8e6655e8065ede13040c2e518e15b1b45c5cd2cf
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-07-27 13:00:15 -0500

    ref

--- application/src/main/java/co/crisi/productopolis/boundaries/output/IBrandRegisterGateway.java
+++ application/src/main/java/co/crisi/productopolis/boundaries/output/IBrandRegisterGateway.java
@@ -1,10 +1,9 @@
 package co.crisi.productopolis.boundaries.output;
 
+import co.crisi.productopolis.boundaries.output.base.IRegisterGateway;
 import co.crisi.productopolis.domain.IBrand;
 
-public interface IBrandRegisterGateway extends IBrandGateway {
-
-    void save(IBrand brand);
+public interface IBrandRegisterGateway extends IRegisterGateway<Long, IBrand> {
 
 
 }
