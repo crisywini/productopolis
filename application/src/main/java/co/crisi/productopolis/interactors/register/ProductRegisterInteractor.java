fc7f8ba5973066e1f632090146d8a36e2af1aa83
Cristian Giovanny Sánchez Pineda <harmaharcri@hotmail.com>
2023-08-01 16:25:19 -0500

    Added either and implemented in product register

--- application/src/main/java/co/crisi/productopolis/interactors/register/ProductRegisterInteractor.java
+++ application/src/main/java/co/crisi/productopolis/interactors/register/ProductRegisterInteractor.java
@@ -5,6 +5,7 @@ import co.crisi.productopolis.boundaries.output.IAttributeExtractGateway;
 import co.crisi.productopolis.boundaries.output.IBrandExtractGateway;
 import co.crisi.productopolis.boundaries.output.ICategoryExtractGateway;
 import co.crisi.productopolis.boundaries.output.IProductRegisterGateway;
+import co.crisi.productopolis.domain.IProduct;
 import co.crisi.productopolis.domain.factory.IProductFactory;
 import co.crisi.productopolis.exception.AttributeNotFoundException;
 import co.crisi.productopolis.exception.BrandNotFoundException;
@@ -15,6 +16,7 @@ import co.crisi.productopolis.model.request.ProductRequest;
 import co.crisi.productopolis.model.response.ProductResponse;
 import co.crisi.productopolis.model.response.mapper.ProductMapper;
 import co.crisi.productopolis.presenter.register.IProductRegisterPresenter;
+import io.vavr.control.Either;
 import lombok.RequiredArgsConstructor;
 import org.mapstruct.factory.Mappers;
 
@@ -38,34 +40,67 @@ public class ProductRegisterInteractor implements IProductRegisterBoundary {
 
     @Override
     public ProductResponse create(ProductRequest request) throws BusinessException {
+        var productExistenceEither = validateProductExistence(request)
+                .flatMap(this::validateBrandExistence)
+                .flatMap(this::validateAttributesExistence)
+                .flatMap(this::validateCategoriesExistence);
 
-        if (gateway.existsById(request.id())) {
-            return presenter.prepareFailView(
-                    new RepeatedProductException(String.format("Product with id %d already exists!", request.id())));
+        if (productExistenceEither.isLeft()) {
+            return presenter.prepareFailView(productExistenceEither.getLeft());
         }
+        var req = productExistenceEither.right().get();
+        var productEither = createProduct(req);
+
+        if (productEither.isRight()) {
+            return presenter.prepareSuccessfulView(productMapper.map(productEither.get()));
+        }
+        return presenter.prepareFailView(productEither.getLeft());
+
+    }
+
+    private Either<BusinessException, ProductRequest> validateProductExistence(ProductRequest request) {
+        if (gateway.existsById(request.id())) {
+            return Either.left(new RepeatedProductException(String
+                    .format("Product with id %d already exists!", request.id())));
+        }
+        return Either.right(request);
+    }
+
+    private Either<BusinessException, ProductRequest> validateBrandExistence(ProductRequest request) {
         if (!brandExtractGateway.existsById(request.brandId())) {
-            return presenter.prepareFailView(new BrandNotFoundException(
+            return Either.left(new BrandNotFoundException(
                     String.format("The brand with id %d was not found!", request.brandId())));
         }
+        return Either.right(request);
+    }
 
+    private Either<BusinessException, ProductRequest> validateAttributesExistence(ProductRequest request) {
         var attIds = request.attributeIds().stream()
                 .allMatch(attributeExtractGateway::existsById);
 
         if (!attIds) {
             var idsNotFound = request.attributeIds().stream()
                     .filter(id -> !attributeExtractGateway.existsById(id)).toList();
-            return presenter.prepareFailView(
+            return Either.left(
                     new AttributeNotFoundException("The ids " + idsNotFound + " were not found!"));
         }
+        return Either.right(request);
+    }
+
+    private Either<BusinessException, ProductRequest> validateCategoriesExistence(ProductRequest request) {
         var catIds = request.categoryIds().stream()
                 .allMatch(categoryExtractGateway::existsById);
 
         if (!catIds) {
             var idsNotFound = request.categoryIds().stream()
                     .filter(id -> !categoryExtractGateway.existsById(id)).toList();
-            return presenter.prepareFailView(
+            return Either.left(
                     new CategoryNotFoundException("The ids " + idsNotFound + " were not found!"));
         }
+        return Either.right(request);
+    }
+
+    private Either<BusinessException, IProduct> createProduct(ProductRequest request) {
         var brand = brandExtractGateway.getById(request.brandId());
 
         var attributes = request.attributeIds().stream()
@@ -78,13 +113,10 @@ public class ProductRegisterInteractor implements IProductRegisterBoundary {
             var product = factory.create(request.id(), request.name(), request.description(), request.price(),
                     request.stock(),
                     request.isFeatured(), request.isActive(), brand, attributes, categories);
-            var productSaved = gateway.save(product);
-            return presenter.prepareSuccessfulView(productMapper.map(productSaved));
+            return Either.right(product);
         } catch (RuntimeException e) {
-            return presenter.prepareFailView(new BusinessException(e.getMessage()));
+            return Either.left(new BusinessException(e.getMessage()));
         }
-
-
     }
 
 }
---
+++ application/src/test/java/co/crisi/productopolis/interactors/register/ProductRegisterInteractorTest.java
@@ -0,0 +1,12 @@
+package co.crisi.productopolis.interactors.register;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRegisterInteractorTest {
+
+    @Test
+    void create() {
+    }
+}
