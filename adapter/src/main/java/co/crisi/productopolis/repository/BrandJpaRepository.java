0a62dbfc864e27e911cd6e04a494001e77761e0f
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-08-03 17:57:02 -0500

    added jpa respositories

--- adapter/src/main/java/co/crisi/productopolis/repository/BrandJpaRepository.java
+++ adapter/src/main/java/co/crisi/productopolis/repository/BrandJpaRepository.java
@@ -1,12 +1,11 @@
 package co.crisi.productopolis.repository;
 
-import co.crisi.productopolis.gateway.jpamodel.ProductJpaEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductJpaRepository extends PagingAndSortingRepository<ProductJpaEntity, Long>,
-        CrudRepository<ProductJpaEntity, Long> {
+public interface BrandJpaRepository extends PagingAndSortingRepository<BrandJpaRepository, Long>,
+        CrudRepository<BrandJpaRepository, Long> {
 
 }
