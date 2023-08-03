e8d6f7c3caa0a4fb94984a2336dab0da2c222155
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-08-03 16:49:10 -0500

    implemented the orm model

---
+++ adapter/src/main/java/co/crisi/productopolis/gateway/jpamodel/CategoryJpaEntity.java
@@ -0,0 +1,54 @@
+package co.crisi.productopolis.gateway.jpamodel;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "category")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class CategoryJpaEntity {
+
+    @Id
+    @Column(name = "category_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    @Column(name = "creation_date")
+    private LocalDate creationDate;
+
+    @Column(name = "last_update")
+    private LocalDate lastUpdate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CategoryJpaEntity that = (CategoryJpaEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
