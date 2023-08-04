f869a9c0d1c7bfeba02fbf986bd5e848fccf2a25
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-08-04 16:51:05 -0500

    Added product beans config

---
+++ adapter/src/main/java/co/crisi/productopolis/config/BrandBeansConfig.java
@@ -0,0 +1,9 @@
+package co.crisi.productopolis.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BrandBeansConfig {
+
+
+}
---
+++ adapter/src/main/java/co/crisi/productopolis/config/ProductBeansConfig.java
@@ -0,0 +1,37 @@
+package co.crisi.productopolis.config;
+
+import co.crisi.productopolis.boundaries.input.register.IProductRegisterBoundary;
+import co.crisi.productopolis.boundaries.output.IAttributeExtractGateway;
+import co.crisi.productopolis.boundaries.output.IBrandExtractGateway;
+import co.crisi.productopolis.boundaries.output.ICategoryExtractGateway;
+import co.crisi.productopolis.boundaries.output.IProductRegisterGateway;
+import co.crisi.productopolis.gateway.postgresql.register.ProductRegisterPostgresGateway;
+import co.crisi.productopolis.interactors.register.ProductRegisterInteractor;
+import co.crisi.productopolis.presenter.register.IProductRegisterPresenter;
+import co.crisi.productopolis.presenter.register.ProductRegisterPresenter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProductBeansConfig {
+
+    @Bean
+    public IProductRegisterGateway productRegisterGateway() {
+        return new ProductRegisterPostgresGateway();
+    }
+
+    @Bean
+    public IProductRegisterPresenter productRegisterPresenter() {
+        return new ProductRegisterPresenter();
+    }
+
+    @Bean
+    public IProductRegisterBoundary productRegisterBoundary(IProductRegisterGateway gateway,
+            IProductRegisterPresenter presenter, IBrandExtractGateway brandExtractGateway,
+            IAttributeExtractGateway attributeExtractGateway, ICategoryExtractGateway categoryExtractGateway) {
+        return new ProductRegisterInteractor(gateway, presenter, brandExtractGateway, attributeExtractGateway,
+                categoryExtractGateway);
+    }
+
+
+}
