8181a52d3c75cbe766917da32c579d6ede162693
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-08-30 08:45:59 -0500

    Added brand update gateway impl

--- adapter/src/main/java/co/crisi/productopolis/gateway/postgresql/update/BrandUpdatePostgresGateway.java
+++ adapter/src/main/java/co/crisi/productopolis/gateway/postgresql/update/BrandUpdatePostgresGateway.java
@@ -2,24 +2,32 @@ package co.crisi.productopolis.gateway.postgresql.update;
 
 import co.crisi.productopolis.boundaries.output.IBrandUpdateGateway;
 import co.crisi.productopolis.domain.IBrand;
+import co.crisi.productopolis.gateway.jpamodel.mapper.BrandJpaMapper;
 import co.crisi.productopolis.repository.BrandJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+
+@RequiredArgsConstructor
 @Repository
 public class BrandUpdatePostgresGateway implements IBrandUpdateGateway {
 
-    @Autowired
-    private BrandJpaRepository repository;
+    private final BrandJpaRepository repository;
+
+    private final BrandJpaMapper mapper = Mappers.getMapper(BrandJpaMapper.class);
 
     @Override
-    public boolean existsById(Long aLong) {
-        return false;
+    public boolean existsById(Long id) {
+        return repository.existsById(id);
     }
 
     @Override
-    public IBrand update(Long aLong, IBrand newInfo) {
-        return null;
+    public IBrand update(Long id, IBrand newInfo) {
+        var entity = mapper.map(newInfo);
+        var entityUpdated = repository.save(entity);
+        return mapper.map(entityUpdated);
     }
 
 }
