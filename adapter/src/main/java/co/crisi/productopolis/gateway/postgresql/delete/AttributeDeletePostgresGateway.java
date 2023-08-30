3999cc856549ac64f64922fd4f8df8d9df99c51a
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-08-30 09:11:00 -0500

    Added attribute delete posgres gateway

--- adapter/src/main/java/co/crisi/productopolis/gateway/postgresql/delete/AttributeDeletePostgresGateway.java
+++ adapter/src/main/java/co/crisi/productopolis/gateway/postgresql/delete/AttributeDeletePostgresGateway.java
@@ -1,24 +1,28 @@
 package co.crisi.productopolis.gateway.postgresql.delete;
 
 import co.crisi.productopolis.boundaries.output.IAttributeDeleteGateway;
+import co.crisi.productopolis.gateway.jpamodel.mapper.AttributeJpaMapper;
 import co.crisi.productopolis.repository.AttributeJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+@RequiredArgsConstructor
 @Repository
 public class AttributeDeletePostgresGateway implements IAttributeDeleteGateway {
 
-    @Autowired
-    private AttributeJpaRepository repository;
+    private final AttributeJpaRepository repository;
+
 
     @Override
-    public void deleteById(Long aLong) {
-
+    public void deleteById(Long id) {
+        repository.deleteById(id);
     }
 
     @Override
-    public boolean existsById(Long aLong) {
-        return false;
+    public boolean existsById(Long id) {
+        return repository.existsById(id);
     }
 
 }
