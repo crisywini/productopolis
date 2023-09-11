a094ca7b748412d7469756208c75fa67b9d2f94a
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-09-10 22:30:38 -0500

    Added register doc

--- adapter/src/main/java/co/crisi/productopolis/controller/register/ProductRegisterController.java
+++ adapter/src/main/java/co/crisi/productopolis/controller/register/ProductRegisterController.java
@@ -3,7 +3,11 @@ package co.crisi.productopolis.controller.register;
 import co.crisi.productopolis.boundaries.input.register.IProductRegisterBoundary;
 import co.crisi.productopolis.model.request.ProductRequest;
 import co.crisi.productopolis.model.response.ProductResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,10 +22,16 @@ public class ProductRegisterController {
     private final IProductRegisterBoundary boundary;
 
     @PostMapping
+    @Operation(summary = "Creates a new Product")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "201", description = "Product Created")
+            }
+    )
     public ResponseEntity<ProductResponse> create(
             @RequestBody
                     ProductRequest request) {
-        return ResponseEntity.ok(boundary.create(request));
+        return new ResponseEntity<>(boundary.create(request), HttpStatus.CREATED);
     }
 
 }
