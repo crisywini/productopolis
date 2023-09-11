7875400113d6b6dca0e3beae86d20be082c713f0
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-09-10 22:24:25 -0500

    Added doc for register attribute

--- adapter/src/main/java/co/crisi/productopolis/controller/register/AttributeRegisterController.java
+++ adapter/src/main/java/co/crisi/productopolis/controller/register/AttributeRegisterController.java
@@ -3,7 +3,11 @@ package co.crisi.productopolis.controller.register;
 import co.crisi.productopolis.boundaries.input.register.IAttributeRegisterBoundary;
 import co.crisi.productopolis.model.request.AttributeRequest;
 import co.crisi.productopolis.model.response.AttributeResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,11 +21,17 @@ public class AttributeRegisterController {
 
     private final IAttributeRegisterBoundary boundary;
 
+    @Operation(summary = "Creates an attribute")
     @PostMapping
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "201", description = "Entity Created")
+            }
+    )
     public ResponseEntity<AttributeResponse> create(
             @RequestBody
                     AttributeRequest request) {
-        return ResponseEntity.ok(boundary.create(request));
+        return new ResponseEntity<>(boundary.create(request), HttpStatus.CREATED);
     }
 
 }
